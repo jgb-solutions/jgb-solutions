6767095cebc8d4165e5ded0550939a3a72f47a39
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2026-01-27 15:37:21 -0500

    updated vite to default of 7

---
+++ eslint.config.ts
@@ -0,0 +1,13 @@
+import pluginRouter from "@tanstack/eslint-plugin-router"
+
+export default [
+	{
+		plugins: {
+			"@tanstack/router": pluginRouter
+		},
+		rules: {
+			"@tanstack/router/create-route-property-order": "error"
+		}
+	}
+	// Any other config...
+]
